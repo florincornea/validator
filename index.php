84b3902ca29f249a42c0e7f309de6588a643d5d5
Cornea Florin <47784897+florincornea@users.noreply.github.com>
2020-02-04 22:36:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
+require_once ('./Validator/Validator.php'); 
 
-require('./Validator/Validator.php'); 
-
-if (strip_tags($_POST["name"])) {
-    $validator = new Validator();
-    
-    $nameMessages = [
-            'required' => "Campul nume este obligatoriu",
-            'min:3' => "Campul nume trebuie sa aiba minim 3 caractere",
-            'max:10' => "Campul nume trebuie sa aiba maxim 10 caractere",
-        ];
+$validator = new Validator();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $emailMessages = [
             'required' => "Campul email este obligatoriu",
-            'email' => "Campul email trebuie sa fie de tip email",
+            'email' => "Campul email trebuie sa contina un email valid",
         ];
-    
-    $validName = $validator::validate(
-        $_POST["name"],
-        'required|min:3|max:10',
-        $nameMessages
-    );
-    
-    var_dump($validName);
+    if (array_key_exists('name', $_POST)) {
+        $nameMessages = [
+            'required' => "Campul nume este obligatoriu",
+            'min' => "Campul nume trebuie sa aiba minim 5 caractere",
+            'max' => "Campul nume trebuie sa aiba maxim 10 caractere",
+        ];
+
+        $validName = $validator::validate(
+            'name',
+            $_POST["name"],
+            'required|min:5|max:10',
+            $nameMessages
+        );
+    }
 }
 
 ?>
@@ -30,10 +29,17 @@ if (strip_tags($_POST["name"])) {
         <title>validator</title>
     </head>
     <body>
-        <form method="POST" action="/index.php">
-            <input type="text" name="name">
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <input type="text" name="name" value="<?php echo isset($validName) ? $validName : '';?>">
             <input type="email" name="email">
             <input type="submit" value="Submit">
+            <?php
+                if ($errors = $validator::fails()) {
+                    foreach ($errors as $error) {
+                        echo "<div>$error</div>";
+                    }
+                }
+            ?>
         </form>
     </body>
 </html>
