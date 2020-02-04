2988bc8b5fdee8ff611a4c36a58147b70f92445e
Cornea Florin <47784897+florincornea@users.noreply.github.com>
2020-02-04 22:36:44 +0200

    Update RulesFactory.php

--- Validator/RulesFactory.php
+++ Validator/RulesFactory.php
@@ -1,20 +1,46 @@
 <?php
+require_once ('Rules/RequiredRule.php');
+require_once ('Rules/MinRule.php');
+require_once ('Rules/MaxRule.php');
 
-class RulesFactory {
-    public static function selectRule($rule, $field) {
+class RulesFactory
+{
+    public static function selectRule($fieldName, $fieldVal, $rule, $message, $limit)
+    {
         switch ($rule) {
             case 'required':
+                $rule = new RequiredRule();
 
-                exit($field);
-                return new RequiredRule();
+                $rule->validateRule(
+                    $fieldName,
+                    $fieldVal,
+                    $message,
+                    $limit
+                );
                 break;
             case 'email':
 
                 return new EmailRule();
                 break;
             case 'min':
+                $rule = new MinRule();
 
-                return new MinRule();
+                $rule->validateRule(
+                    $fieldName,
+                    $fieldVal,
+                    $message,
+                    $limit
+                );
+                break;
+            case 'max':
+                $rule = new MaxRule();
+
+                $rule->validateRule(
+                    $fieldName,
+                    $fieldVal,
+                    $message,
+                    $limit
+                );
                 break;
         }
     }
